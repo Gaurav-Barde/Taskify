cbc72bb4f3810c561e4f970921afa307d9f25723
Gaurav-Barde <gabarde25@gmail.com>
2023-07-20 16:36:37 +0530

    Added required styles &  imported necessary comp's

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-function App() {
+import { useState } from "react";
+import TaskInputField from "./components/TaskInputField";
+import TaskList from "./components/TaskList";
+import { Task } from "./model";
+
+const App: React.FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const handleAddTask = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (task) {
+      setTasks([...tasks, { id: Date.now(), task: task, isComplete: false }]);
+    }
+    setTask("");
+  };
 
   return (
-    <>
-      <h1>Hello React with Typescript!</h1>
-    </>
-  )
-}
+    <div className="flex flex-col flex-1 h-full items-center py-8 px-4 bg-customDark">
+      <h1 className="text-white text-7xl mb-8">Taskify</h1>
+      <TaskInputField
+        task={task}
+        setTask={setTask}
+        handleAddTask={handleAddTask}
+      />
+      <TaskList tasks={tasks} setTasks={setTasks} />
+    </div>
+  );
+};
 
-export default App
+export default App;
