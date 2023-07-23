0bbe5898ba4ce98dba34968cb1821137678f9b1e
Gaurav-Barde <gabarde25@gmail.com>
2023-07-23 15:55:40 +0530

    Centered Loader

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -1,6 +1,9 @@
 const Loader = () => {
   return (
-    <div role="status">
+    <div
+      role="status"
+      className="w-screen h-screen flex items-center justify-center"
+    >
       <svg
         aria-hidden="true"
         className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
