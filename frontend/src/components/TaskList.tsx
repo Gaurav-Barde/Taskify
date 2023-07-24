6281f977d0d5c49b5c652eed051355cc83164e1b
Gaurav-Barde <gabarde25@gmail.com>
2023-07-24 14:27:08 +0530

    Added and Sent required props

--- frontend/src/components/TaskList.tsx
+++ frontend/src/components/TaskList.tsx
@@ -5,18 +5,28 @@ import TaskListItem from "./TaskListItem";
 interface Props {
   tasks: Task[];
   setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
+  handleDeleteTask: (id: number) => Promise<void>;
+  getTasks: () => void;
 }
 
-const TaskList = memo(({ tasks, setTasks }: Props) => {
-  return (
-    tasks.length && (
-      <ul className="w-3/4 text-white my-8 bg-slate-800 border border-slate-600 rounded-md">
-        {tasks.map((task) => (
-          <TaskListItem key={task.id} task={task} setTasks={setTasks} />
-        ))}
-      </ul>
-    )
-  );
-});
+const TaskList = memo(
+  ({ tasks, setTasks, handleDeleteTask, getTasks }: Props) => {
+    return (
+      tasks.length && (
+        <ul className="w-3/4 text-white my-8 bg-slate-800 border border-slate-600 rounded-md">
+          {tasks.map((task) => (
+            <TaskListItem
+              key={task.id}
+              tasks={tasks}
+              task={task}
+              handleDeleteTask={handleDeleteTask}
+              getTasks={getTasks}
+            />
+          ))}
+        </ul>
+      )
+    );
+  }
+);
 
 export default TaskList;
